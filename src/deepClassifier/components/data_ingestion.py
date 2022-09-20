8d76ce88e048a4e8fdfe0b0c72dc00378cd38704
Akarsh-KN <akarshkn136259@gmail.com>
2022-09-20 10:43:18 +0530

    formatted with black

--- src/deepClassifier/components/data_ingestion.py
+++ src/deepClassifier/components/data_ingestion.py
@@ -5,39 +5,46 @@ from urllib import request
 from tqdm import tqdm
 from deepClassifier.entity import DataIngestionConfig
 
+
 class DownloadProgressBar(tqdm):
     def update_to(self, b=1, bsize=1, tsize=None):
         if tsize is not None:
             self.total = tsize
         self.update(b * bsize - self.n)
 
+
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
-        self.config=config
-    
+        self.config = config
+
     def download_file(self):
         if not os.path.exists(self.config.local_data_file):
             # with DownloadProgressBar(unit='B', unit_scale=True,
             #                  miniters=1, desc=self.config.source_url.split('/')[-1]) as t:
             #                 request.urlretrieve(self.config.source_url,filename=self.config.local_data_file,
             #                  reporthook=t.update_to)
-            filename, header = request.urlretrieve(self.config.source_url,filename=self.config.local_data_file)
-    
+            filename, header = request.urlretrieve(
+                self.config.source_url, filename=self.config.local_data_file
+            )
+
     def _get_updated_list_of_files(self, list_of_files):
-        return [f for f in list_of_files if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)]
+        return [
+            f
+            for f in list_of_files
+            if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)
+        ]
 
     def _preprocess(self, zf: ZipFile, f: str, working_dir: str):
-        target_filepath=os.path.join(working_dir, f)
+        target_filepath = os.path.join(working_dir, f)
         if not os.path.exists(target_filepath):
             zf.extract(f, working_dir)
-        
+
         if os.path.getsize(target_filepath) == 0:
             os.remove(target_filepath)
 
-    
     def unzip_and_clean(self):
         with ZipFile(file=self.config.local_data_file, mode="r") as zf:
-            list_of_files=zf.namelist()
-            updated_list_of_files=self._get_updated_list_of_files(list_of_files)
+            list_of_files = zf.namelist()
+            updated_list_of_files = self._get_updated_list_of_files(list_of_files)
             for f in updated_list_of_files:
                 self._preprocess(zf, f, self.config.unzip_dir)
